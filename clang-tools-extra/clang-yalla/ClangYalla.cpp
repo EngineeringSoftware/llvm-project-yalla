eb1811e314bbfb882f1ab31a2b5a335d93a9b2da
Nader Al Awar <naderalawar@gmail.com>
2024-05-05 19:37:36 -0500

    [clang-yalla] Add initial source code

---
+++ clang-tools-extra/clang-yalla/ClangYalla.cpp
@@ -0,0 +1,192 @@
+#include <iostream>
+#include <string>
+
+#include "clang/ASTMatchers/ASTMatchers.h"
+#include "clang/ASTMatchers/ASTMatchFinder.h"
+// Declares clang::SyntaxOnlyAction.
+#include "clang/Frontend/FrontendActions.h"
+#include "clang/Tooling/CommonOptionsParser.h"
+#include "clang/Tooling/Tooling.h"
+// Declares llvm::cl::extrahelp.
+#include "llvm/Support/CommandLine.h"
+
+using namespace clang::tooling;
+using namespace llvm;
+
+// Apply a custom category to all command-line options so that they are the
+// only ones displayed.
+static llvm::cl::OptionCategory MyToolCategory("my-tool options");
+
+// CommonOptionsParser declares HelpMessage with a description of the common
+// command-line options related to the compilation database and input files.
+// It's nice to have this help message in all tools.
+static cl::extrahelp CommonHelp(CommonOptionsParser::HelpMessage);
+
+// A help message for this specific tool can be added afterwards.
+static cl::extrahelp MoreHelp("\nMore help text...\n");
+
+using namespace clang;
+using namespace clang::ast_matchers;
+
+struct ClassInfo {
+  std::string Name;
+  std::string FileName;
+  bool HasDefinition;
+
+  ClassInfo(std::string Name, std::string FileName, bool HasDefinition) : Name(std::move(Name)), FileName(std::move(FileName)), HasDefinition(HasDefinition) {}
+};
+
+StatementMatcher LoopMatcher =
+  forStmt(hasLoopInit(declStmt(hasSingleDecl(varDecl(
+    hasInitializer(integerLiteral(equals(0)))))))).bind("forLoop");
+
+// StatementMatcher FieldMatcher =
+//  declStmt(
+//   containsDeclaration(0,
+//     fieldDecl(
+//       // hasType(
+//       //   asString("Container")
+//       //   )
+//       )
+//     )
+//   ).bind("ContainerType");
+
+DeclarationMatcher FieldMatcher =
+    fieldDecl(
+      hasType(
+        asString("Container")
+        )
+      ).bind("ContainerType");
+
+// ABOVE MATCHES CONTAINER, BUT NOT REFERENCE OR POINTER
+// DeclarationMatcher ReferenceFieldMatcher =
+//     fieldDecl(
+//       hasTypeLoc(referenceTypeLoc(loc(
+//         asString("Container")
+//         )
+//       ))).bind("ContainerType2");
+
+// This works for references
+DeclarationMatcher ReferenceFieldMatcher =
+    fieldDecl(
+      hasType(
+        asString("Container &")
+        )
+      ).bind("ContainerType2");
+
+// This works for pointers
+DeclarationMatcher PointerFieldMatcher =
+    fieldDecl(
+      hasType(
+        asString("Container *")
+        )
+      ).bind("ContainerType3");
+
+DeclarationMatcher ClassMatcher = cxxRecordDecl().bind("ClassDeclaration");
+DeclarationMatcher ClassUsageMatcher = anyOf(
+  fieldDecl(hasType(cxxRecordDecl(isClass()))).bind("Field"),
+  varDecl(hasType(cxxRecordDecl(isClass()))).bind("Variable"),
+  parmVarDecl(hasType(cxxRecordDecl(isClass()))).bind("Parameter")
+);
+
+// Find all fields, vars, and params using those classes
+// Also find methods and 
+
+// DeclarationMatcher PointerFieldMatcher =
+//     fieldDecl(
+//       hasType(
+//         asString("Container")
+//         )
+//       ).bind("ContainerType");
+
+// Matcher<Decl> = hasType(asString("Container")).bind("ContainerType");
+
+
+class LoopPrinter : public MatchFinder::MatchCallback {
+public :
+  virtual void run(const MatchFinder::MatchResult &Result) {
+    std::cout << "maybe this loop\n";
+    if (const ForStmt *FS = Result.Nodes.getNodeAs<clang::ForStmt>("forLoop"))
+      FS->dump();
+  }
+};
+
+
+class YallaMatcher : public MatchFinder::MatchCallback {
+public:
+  virtual void run(const MatchFinder::MatchResult &Result) {
+    if (const FieldDecl *FD = Result.Nodes.getNodeAs<clang::FieldDecl>("Field")) {
+      std::cout << "Got field " << FD->getNameAsString() << '\n';
+      // std::cout << "one\n";
+      // FD->dump();
+    }
+
+    if (const VarDecl *VD = Result.Nodes.getNodeAs<clang::VarDecl>("Variable")) {
+      std::cout << "Got variable " << VD->getNameAsString() << '\n';
+      // std::cout << "two\n";
+      // FD->dump();
+    }
+
+    if (const ParmVarDecl *PD = Result.Nodes.getNodeAs<clang::ParmVarDecl>("Parameter")) {
+      std::cout << "Got parm " << PD->getNameAsString() << '\n';
+    }
+
+    if (const CXXRecordDecl *RD = Result.Nodes.getNodeAs<clang::CXXRecordDecl>("ClassDeclaration")) {
+      AddClassInfo(RD, Result.Context);
+      // RD->dump();
+    }
+
+    if (const CXXRecordDecl *RD = Result.Nodes.getNodeAs<clang::CXXRecordDecl>("ClassUsage")) {
+      std::cout << "Found usage " << RD->getNameAsString() << '\n';
+    }
+
+  }
+
+  void PrintClasses() const {
+    for (const auto& [Name, CI] : Classes) {
+      std::cout << CI.Name << " " << CI.FileName << " " << CI.HasDefinition << '\n';
+    }
+  }
+
+private:
+  std::unordered_map<std::string, ClassInfo> Classes;
+
+  void AddClassInfo(const CXXRecordDecl *RD, const ASTContext *Context) {
+    std::string Name = RD->getNameAsString();
+
+    // From
+    // https://stackoverflow.com/questions/25075001/how-can-i-get-the-name-of-the-file-im-currently-visiting-with-clang
+    // on how to get source file
+    const SourceManager& SrcMgr = Context->getSourceManager();
+    const FileEntry* Entry = SrcMgr.getFileEntryForID(SrcMgr.getFileID(RD->getLocation()));
+    std::string FileName = Entry->getName().str();
+
+    auto [CI, NewlyInserted] = Classes.try_emplace(Name, Name, FileName, RD->hasDefinition());
+    if (!NewlyInserted) {
+      CI->second.HasDefinition |= RD->hasDefinition();
+    }
+  }
+};
+
+int main(int argc, const char **argv) {
+  auto ExpectedParser = CommonOptionsParser::create(argc, argv, MyToolCategory);
+  if (!ExpectedParser) {
+    // Fail gracefully for unsupported options.
+    llvm::errs() << ExpectedParser.takeError();
+    return 1;
+  }
+  CommonOptionsParser& OptionsParser = ExpectedParser.get();
+  ClangTool Tool(OptionsParser.getCompilations(),
+                 OptionsParser.getSourcePathList());
+
+  YallaMatcher YM;
+  MatchFinder Finder;
+  Finder.addMatcher(ClassMatcher, &YM);
+  Finder.addMatcher(ClassUsageMatcher, &YM);
+
+  auto result = Tool.run(newFrontendActionFactory(&Finder).get());
+
+  YM.PrintClasses();
+
+  return result;
+}
