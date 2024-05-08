338db561528acc04cd82b6502a1e55e561c70ab0
Nader Al Awar <naderalawar@gmail.com>
2024-05-07 19:34:39 -0500

    [clang-yalla] Add class and function usage information, account for reference and pointer types, and ignore implicit RecordDecl nodes

--- clang-tools-extra/clang-yalla/ClangYalla.cpp
+++ clang-tools-extra/clang-yalla/ClangYalla.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <string>
+#include <unordered_map>
+#include <unordered_set>
 
-#include "clang/ASTMatchers/ASTMatchers.h"
 #include "clang/ASTMatchers/ASTMatchFinder.h"
-// Declares clang::SyntaxOnlyAction.
+#include "clang/ASTMatchers/ASTMatchers.h"
 #include "clang/Frontend/FrontendActions.h"
 #include "clang/Tooling/CommonOptionsParser.h"
-#include "clang/Tooling/Tooling.h"
 #include "clang/Tooling/Refactoring.h"
-// Declares llvm::cl::extrahelp.
+#include "clang/Tooling/Tooling.h"
 #include "llvm/Support/CommandLine.h"
 
 using namespace clang::tooling;
@@ -26,15 +26,52 @@ static cl::extrahelp CommonHelp(CommonOptionsParser::HelpMessage);
 // A help message for this specific tool can be added afterwards.
 static cl::extrahelp MoreHelp("\nMore help text...\n");
 
+static llvm::cl::opt<std::string>
+    HeaderCLI("header", llvm::cl::desc("The header to be substituted"),
+              llvm::cl::value_desc("PATH_TO_HEADER"), llvm::cl::Required);
+
 using namespace clang;
 using namespace clang::ast_matchers;
+
+struct TypeScopes {
+  enum class ScopeType {
+    ClassScope,
+    NamespaceScope,
+  };
+
+  std::string Name;
+  ScopeType Type;
+
+  TypeScopes(std::string Name, ScopeType Type)
+      : Name(std::move(Name)), Type(Type) {}
+};
+
+struct ClassUsage {
+  std::string TypeName;
+  bool IsPointer;
+
+  ClassUsage(std::string TypeName, bool IsPointer)
+      : TypeName(std::move(TypeName)), IsPointer(IsPointer) {}
+};
+
+struct FunctionUsage {
+  std::string Name; // TODO: This is redundant for now, but I'm keeping it
+                    // because I need to store source info
+
+  FunctionUsage(std::string Name) : Name(std::move(Name)) {}
+};
 
 struct ClassInfo {
   std::string Name;
   std::string FileName;
   bool HasDefinition;
+  std::vector<TypeScopes> Scopes;
+  std::vector<ClassUsage> Usages;
 
-  ClassInfo(std::string Name, std::string FileName, bool HasDefinition) : Name(std::move(Name)), FileName(std::move(FileName)), HasDefinition(HasDefinition) {}
+  ClassInfo(std::string Name, std::string FileName, bool HasDefinition,
+            std::vector<TypeScopes> &&Scopes)
+      : Name(std::move(Name)), FileName(std::move(FileName)),
+        HasDefinition(HasDefinition), Scopes(std::move(Scopes)), Usages() {}
 };
 
 struct FunctionInfo {
@@ -43,149 +80,289 @@ struct FunctionInfo {
   std::string ClassName;
   bool HasDefinition;
   bool IsTemplate;
+  std::vector<TypeScopes> Scopes;
+  std::vector<FunctionUsage> Usages;
 
-  FunctionInfo(std::string Name, std::string FileName, std::string ClassName, bool HasDefinition, bool IsTemplate) : Name(std::move(Name)), FileName(std::move(FileName)), ClassName(std::move(ClassName)), HasDefinition(HasDefinition), IsTemplate(IsTemplate) {}
+  FunctionInfo(std::string Name, std::string FileName, std::string ClassName,
+               bool HasDefinition, bool IsTemplate,
+               std::vector<TypeScopes> &&Scopes)
+      : Name(std::move(Name)), FileName(std::move(FileName)),
+        ClassName(std::move(ClassName)), HasDefinition(HasDefinition),
+        IsTemplate(IsTemplate), Scopes(std::move(Scopes)), Usages() {}
 
   bool IsMethod() const { return ClassName != ""; }
 };
 
-
-DeclarationMatcher ClassMatcher = cxxRecordDecl().bind("ClassDeclaration");
+// Need the isImplicit check since each definition contains an
+// implicit node that references itself.
+DeclarationMatcher ClassMatcher =
+    recordDecl(unless(isImplicit())).bind("ClassDeclaration");
 DeclarationMatcher ClassUsageMatcher = anyOf(
-  fieldDecl(hasType(cxxRecordDecl(isClass()))).bind("Field"),
-  varDecl(hasType(cxxRecordDecl(isClass()))).bind("Variable")
-  // parmVarDecl(hasType(cxxRecordDecl(isClass()))).bind("Parameter") // ParmVarDecl inherits from VarDecl
+    // fieldDecl(hasType(cxxRecordDecl(isClass()))).bind("Field"),
+    // varDecl(hasType(cxxRecordDecl(isClass()))).bind("Variable")
+    fieldDecl(hasType(recordDecl())).bind("Field"),
+    varDecl(hasType(recordDecl())).bind("Variable"),
+    fieldDecl(hasType(pointerType())).bind("Field"),
+    varDecl(hasType(pointerType())).bind("Variable"),
+    fieldDecl(hasType(referenceType())).bind("Field"),
+    varDecl(hasType(referenceType())).bind("Variable")
+    // parmVarDecl(hasType(cxxRecordDecl(isClass()))).bind("Parameter")
+    // ParmVarDecl inherits from VarDecl
 );
 DeclarationMatcher FunctionMatcher = anyOf(
-  // The order matters here. Since I have functionDecl first,
-  // everything after will be bound to functionDecl since they inherit
-  // from it.
-  cxxMethodDecl().bind("Method"), // Has to be first since MethodDecl inherits from FunctionDecl
-  functionDecl().bind("Function")
-  // cxxConstructorDecl().bind("Constructor"), // Inherits from CXXMethodDecl
-  // cxxDestructorDecl().bind("Destructor"), // Inherits from CXXMethodDecl
-  // functionTemplateDecl().bind("FunctionTemplate") // Same info can be retrieved with FunctionDecl
+    // The order matters here. Since I have functionDecl first,
+    // everything after will be bound to functionDecl since they inherit
+    // from it.
+    cxxMethodDecl().bind("Method"), // Has to be first since MethodDecl inherits
+                                    // from FunctionDecl
+    functionDecl().bind("Function")
+    // cxxConstructorDecl().bind("Constructor"), // Inherits from CXXMethodDecl
+    // cxxDestructorDecl().bind("Destructor"), // Inherits from CXXMethodDecl
+    // functionTemplateDecl().bind("FunctionTemplate") // Same info can be
+    // retrieved with FunctionDecl
 );
 StatementMatcher FunctionCallMatcher = callExpr().bind("FunctionCall");
 
-
 class YallaMatcher : public MatchFinder::MatchCallback {
 public:
-  YallaMatcher(const std::string& SourcePath) : SourcePath(SourcePath) {}
+  YallaMatcher(const std::unordered_set<std::string> &SourcePaths,
+               const std::string &HeaderPath)
+      : SourcePaths(SourcePaths), HeaderPath(HeaderPath) {}
 
-  virtual void run(const MatchFinder::MatchResult &Result) {
-    if (const FieldDecl *FD = Result.Nodes.getNodeAs<clang::FieldDecl>("Field")) {
-      std::cout << "Got field " << FD->getNameAsString() << '\n';
-    }
-
-    if (const VarDecl *VD = Result.Nodes.getNodeAs<clang::VarDecl>("Variable")) {
-      std::cout << "Got variable " << VD->getNameAsString() << '\n';
-    }
-
-    if (const ParmVarDecl *PD = Result.Nodes.getNodeAs<clang::ParmVarDecl>("Parameter")) {
-      std::cout << "Got parm " << PD->getNameAsString() << '\n';
-    }
-
-    if (const CXXRecordDecl *RD = Result.Nodes.getNodeAs<clang::CXXRecordDecl>("ClassDeclaration")) {
+  virtual void run(const MatchFinder::MatchResult &Result) override {
+    if (const RecordDecl *RD =
+            Result.Nodes.getNodeAs<clang::RecordDecl>("ClassDeclaration")) {
       std::string FileName = GetContainingFile(RD);
-      if (FileName == SourcePath)
+      if (FileName == HeaderPath)
         AddClassInfo(RD, FileName);
     }
 
-    if (const CXXMethodDecl *MD = Result.Nodes.getNodeAs<clang::CXXMethodDecl>("Method")) {
+    if (const CXXMethodDecl *MD =
+            Result.Nodes.getNodeAs<clang::CXXMethodDecl>("Method")) {
       std::string FileName = GetContainingFile(MD);
-      if (FileName == SourcePath)
+      if (FileName == HeaderPath)
         AddFunctionInfo(MD, FileName, MD->getParent()->getNameAsString());
     }
 
-    if (const FunctionDecl *FD = Result.Nodes.getNodeAs<clang::FunctionDecl>("Function")) {
+    if (const FunctionDecl *FD =
+            Result.Nodes.getNodeAs<clang::FunctionDecl>("Function")) {
       std::string FileName = GetContainingFile(FD);
-      if (FileName == SourcePath)
+      if (FileName == HeaderPath)
         AddFunctionInfo(FD, FileName);
     }
 
-    if (const CallExpr *CE = Result.Nodes.getNodeAs<clang::CallExpr>("FunctionCall")) {
-      std::cout << "Found call expr " << CE->getDirectCallee()->getNameAsString() << '\n';
+    if (const FieldDecl *FD =
+            Result.Nodes.getNodeAs<clang::FieldDecl>("Field")) {
+      std::string FileName = GetContainingFile(FD);
+      if (SourcePaths.find(FileName) != SourcePaths.end()) {
+        AddClassUsage(FD, FileName);
+      }
+    }
+
+    if (const VarDecl *VD =
+            Result.Nodes.getNodeAs<clang::VarDecl>("Variable")) {
+      std::string FileName = GetContainingFile(VD);
+      if (SourcePaths.find(FileName) != SourcePaths.end())
+        AddClassUsage(VD, FileName);
+    }
+
+    if (const CallExpr *CE =
+            Result.Nodes.getNodeAs<clang::CallExpr>("FunctionCall")) {
+      std::string FileName = GetContainingFile(CE->getDirectCallee());
+      if (SourcePaths.find(FileName) != SourcePaths.end())
+        AddFunctionUsage(CE);
     }
   }
 
   void PrintClasses() const {
-    for (const auto& [Name, CI] : Classes) {
-      std::cout << CI.Name << " " << CI.FileName << " " << CI.HasDefinition << '\n';
+    for (const auto &[Name, CI] : Classes) {
+      std::cout << CI.Name << " " << CI.FileName << " " << CI.HasDefinition
+                << '\n';
     }
   }
 
   void PrintFunctions() const {
-    for (const auto& [Name, FI] : Functions) {
-      std::cout << FI.Name << " " << FI.FileName << " " << FI.HasDefinition << " " << FI.ClassName << " " << FI.IsTemplate << '\n';
+    for (const auto &[Name, FI] : Functions) {
+      std::cout << FI.Name << " " << FI.FileName << " " << FI.HasDefinition
+                << " " << FI.ClassName << " " << FI.IsTemplate << '\n';
     }
   }
 
-  std::unordered_map<std::string, ClassInfo>& GetClasses() {
-    return Classes;
-  }
+  std::unordered_map<std::string, ClassInfo> &GetClasses() { return Classes; }
 
 private:
   std::unordered_map<std::string, ClassInfo> Classes;
   std::unordered_map<std::string, FunctionInfo> Functions;
-  const std::string& SourcePath;
+  const std::unordered_set<std::string> &SourcePaths;
+  const std::string &HeaderPath;
 
-  void AddClassInfo(const CXXRecordDecl *RD, const std::string& FileName) {
+  void AddClassInfo(const RecordDecl *RD, const std::string &FileName) {
     std::string Name = RD->getNameAsString();
+    bool HasDefinition = RD->getDefinition() != nullptr;
 
-    auto [CI, NewlyInserted] = Classes.try_emplace(Name, Name, FileName, RD->hasDefinition());
+    auto [Scopes, FullyScopedName] = GetScopes(RD);
+    if (!FullyScopedName.empty())
+      FullyScopedName += "::";
+    FullyScopedName += Name;
+
+    auto [CI, NewlyInserted] = Classes.try_emplace(
+        FullyScopedName, Name, FileName, HasDefinition, std::move(Scopes));
     if (!NewlyInserted) {
-      CI->second.HasDefinition |= RD->hasDefinition();
+      CI->second.HasDefinition |= HasDefinition;
     }
   }
 
-  void AddFunctionInfo(const FunctionDecl *FD, const std::string& FileName, const std::string& ClassName = "") {
+  void AddClassUsage(const ValueDecl *VD, const std::string &FileName) {
+    QualType Type = GetBaseType(VD->getType().getUnqualifiedType());
+
+    auto [Scopes, FullyScopedName] = GetScopes(Type);
+    if (!FullyScopedName.empty())
+      FullyScopedName += "::";
+    FullyScopedName += GetUnscopedName(Type);
+
+    auto it = Classes.find(FullyScopedName);
+    if (it == Classes.end())
+      llvm::report_fatal_error("Found class usage before definition");
+
+    ClassInfo &CI = it->second;
+    CI.Usages.emplace_back(Type.getAsString(),
+                           Type->isReferenceType() || Type->isPointerType());
+  }
+
+  void AddFunctionInfo(const FunctionDecl *FD, const std::string &FileName,
+                       const std::string &ClassName = "") {
     std::string Name = FD->getNameAsString();
 
-    auto [FI, NewlyInserted] = Functions.try_emplace(Name, Name, FileName, ClassName, FD->isDefined(), FD->isTemplated());
+    auto [Scopes, FullyScopedName] = GetScopes(FD);
+    if (!FullyScopedName.empty())
+      FullyScopedName += "::";
+    FullyScopedName += Name;
+
+    auto [FI, NewlyInserted] = Functions.try_emplace(
+        FullyScopedName, Name, FileName, ClassName, FD->isDefined(),
+        FD->isTemplated(), std::move(Scopes));
     if (!NewlyInserted) {
       FI->second.HasDefinition |= FD->isDefined();
     }
   }
 
-  std::string GetContainingFile(const Decl* D) const {
-    const ASTContext& Context = D->getASTContext();
+  void AddFunctionUsage(const CallExpr *CE) {
+    const FunctionDecl *FD = CE->getDirectCallee();
+
+    auto [Scopes, FullyScopedName] = GetScopes(FD);
+    if (!FullyScopedName.empty())
+      FullyScopedName += "::";
+    FullyScopedName += FD->getNameAsString();
+
+    auto it = Functions.find(FullyScopedName);
+    if (it == Functions.end())
+      llvm::report_fatal_error("Found function usage before definition");
+
+    FunctionInfo &FI = it->second;
+    FI.Usages.emplace_back(FD->getNameAsString());
+  }
+
+  std::string GetContainingFile(const Decl *D) const {
+    const ASTContext &Context = D->getASTContext();
     // From
     // https://stackoverflow.com/questions/25075001/how-can-i-get-the-name-of-the-file-im-currently-visiting-with-clang
     // on how to get source file
-    const SourceManager& SrcMgr = Context.getSourceManager();
-    const FileEntry* Entry = SrcMgr.getFileEntryForID(SrcMgr.getFileID(D->getLocation()));
+    const SourceManager &SrcMgr = Context.getSourceManager();
+    const FileEntry *Entry =
+        SrcMgr.getFileEntryForID(SrcMgr.getFileID(D->getLocation()));
     return Entry->getName().str();
+  }
+
+  // Gets the type without & and *
+  QualType GetBaseType(QualType Type) const {
+    while (Type->isReferenceType() || Type->isPointerType()) {
+      Type = Type->getPointeeType();
+    }
+
+    return Type;
+  }
+
+  std::pair<std::vector<TypeScopes>, std::string>
+  GetScopes(QualType Type) const {
+    const RecordType *RT = Type->getAs<RecordType>();
+    if (RT == nullptr)
+      llvm::report_fatal_error("RecordType cannot be nullptr");
+
+    const RecordDecl *RD = RT->getDecl();
+    return GetScopes(RD);
+  }
+
+  std::pair<std::vector<TypeScopes>, std::string>
+  GetScopes(const Decl *D) const {
+    std::vector<TypeScopes> Scopes;
+    const DeclContext *DC = D->getDeclContext();
+
+    while (DC && !DC->isTranslationUnit()) {
+      if (const NamespaceDecl *NS = dyn_cast<NamespaceDecl>(DC)) {
+        Scopes.emplace(Scopes.begin(), NS->getNameAsString(),
+                       TypeScopes::ScopeType::NamespaceScope);
+      } else if (const RecordDecl *RD = dyn_cast<RecordDecl>(DC)) {
+        Scopes.emplace(Scopes.begin(), RD->getNameAsString(),
+                       TypeScopes::ScopeType::ClassScope);
+      } else {
+        llvm_unreachable("Scope can only be namespace or class");
+      }
+
+      DC = DC->getParent();
+    }
+
+    std::string FullyScopedName;
+    for (const auto &Scope : Scopes) {
+      if (!FullyScopedName.empty())
+        FullyScopedName += "::";
+      FullyScopedName += Scope.Name;
+    }
+
+    return {Scopes, FullyScopedName};
+  }
+
+  std::string GetUnscopedName(QualType QT) const {
+    const clang::Type *T = QT.getTypePtr();
+    const RecordType *RT = T->getAs<RecordType>();
+    if (RT == nullptr)
+      llvm::report_fatal_error("RecordType cannot be nullptr");
+
+    return RT->getDecl()->getNameAsString();
   }
 };
 
 int main(int argc, const char **argv) {
   auto ExpectedParser = CommonOptionsParser::create(argc, argv, MyToolCategory);
   if (!ExpectedParser) {
-    // Fail gracefully for unsupported options.
     llvm::errs() << ExpectedParser.takeError();
     return 1;
   }
 
-  CommonOptionsParser& OptionsParser = ExpectedParser.get();
-  const auto& SourcePaths = OptionsParser.getSourcePathList();
+  CommonOptionsParser &OptionsParser = ExpectedParser.get();
+  const std::vector<std::string> &SourcePathList =
+      OptionsParser.getSourcePathList();
+  std::unordered_set<std::string> SourcePaths;
+  for (const auto &Path : SourcePathList) {
+    SourcePaths.insert(getAbsolutePath(Path));
+  }
 
-  if (SourcePaths.size() != 1) {
-    llvm::errs() << "ClangYalla supports only one input source file\n";
+  if (HeaderCLI.empty()) {
+    llvm::errs() << "ERROR: Header file not specified with -header\n";
     return 1;
   }
 
-  std::string AbsolutePath = getAbsolutePath(SourcePaths[0]);
+  std::string HeaderAbsolutePath = getAbsolutePath(HeaderCLI.getValue());
 
-  YallaMatcher YM(AbsolutePath);
+  RefactoringTool Tool(OptionsParser.getCompilations(),
+                       OptionsParser.getSourcePathList());
+
+  YallaMatcher YM(SourcePaths, HeaderAbsolutePath);
   MatchFinder Finder;
   Finder.addMatcher(ClassMatcher, &YM);
   Finder.addMatcher(ClassUsageMatcher, &YM);
   Finder.addMatcher(FunctionMatcher, &YM);
   Finder.addMatcher(FunctionCallMatcher, &YM);
 
-  RefactoringTool Tool(OptionsParser.getCompilations(),
-                 OptionsParser.getSourcePathList());
   auto result = Tool.run(newFrontendActionFactory(&Finder).get());
 
   YM.PrintClasses();
